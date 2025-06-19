9ae46a7cc41a472e10f5e7d7e8b92fee54a371d2
burnt-exe <raydo@skunkworks.africa>
2025-06-19 23:46:04 +0200

    Describe your changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/yelverton/', // 👈 MUST match the repo name exactly
   plugins: [react()],
 })
